94f47069d1f1aef69f699ce07d2fe2f7c5d97569
fujino <fujino_kohei@uniquevision.co.jp>
2017-09-09 11:35:13 +0900

    first

---
+++ tryJenkins/tryJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tryJenkins
+//
+//  Created by 藤野航平 on 2017/09/08.
+//  Copyright © 2017年 藤野航平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
